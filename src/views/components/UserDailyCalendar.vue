17581f22ca074ccd6b198b5558c17131d00fb36c
Xxings <10years.shimhyeonggwan@gmail.com>
2020-02-18 14:27:16 +0900

    DashBoard Start

--- src/views/components/UserDailyCalendar.vue
+++ src/views/components/UserDailyCalendar.vue
@@ -1,29 +1,49 @@
 
 <template>
-  <CCardBody>
-    <CRow>
-      <CCol sm="5">
-        <h4 id="traffic" class="card-title mb-0">Traffic</h4>
-        <div class="small text-muted">November 2017</div>
-      </CCol>
-      <CCol sm="7" class="d-none d-md-block">
-        <CButton color="primary" class="float-right">
-          <CIcon name="cil-cloud-download" />
-        </CButton>
-        <CButtonGroup class="float-right mr-3">
-          <CButton
-            color="outline-secondary"
-            v-for="(value, key) in ['Day', 'Month', 'Year']"
-            :key="key"
-            class="mx-0"
-            :pressed="value === selected ? true : false"
-            @click="selected = value"
-          >{{value}}</CButton>
-        </CButtonGroup>
-      </CCol>
-    </CRow>
-    <MainChartExample style="height:300px;margin-top:40px;" />
-  </CCardBody>
+  <CRow>
+    <CCol md="12">
+      <CCard>
+        <CCardBody>
+          <CRow>
+            <CCol sm="5">
+              <h4 id="traffic" class="card-title mb-0">DailyChart</h4>
+              <div class="small text-muted">November 2017 ~ November 2017</div>
+            </CCol>
+          </CRow>
+          <CCard style="height:300px;margin-top:40px">DailyChart가 들어감</CCard>
+        </CCardBody>
+        <CCardFooter>
+          <CRow class="text-center">
+            <CCol md sm="12" class="mb-sm-2 mb-0">
+              <div class="text-muted">제출수</div>
+              <strong>29.703 Users (40%)</strong>
+              <CProgress class="progress-xs mt-2" :precision="1" color="success" :value="40" />
+            </CCol>
+            <CCol md sm="12" class="mb-sm-2 mb-0 d-md-down-none">
+              <div class="text-muted">성공률</div>
+              <strong>24.093 Users (20%)</strong>
+              <CProgress class="progress-xs mt-2" :precision="1" color="info" :value="20" />
+            </CCol>
+            <CCol md sm="12" class="mb-sm-2 mb-0">
+              <div class="text-muted">맞은문제</div>
+              <strong>78.706 Views (60%)</strong>
+              <CProgress class="progress-xs mt-2" :precision="1" color="warning" :value="60" />
+            </CCol>
+            <CCol md sm="12" class="mb-sm-2 mb-0">
+              <div class="text-muted">시도했지만 못푼문제</div>
+              <strong>22.123 Users (80%)</strong>
+              <CProgress class="progress-xs mt-2" :precision="1" color="danger" :value="80" />
+            </CCol>
+            <CCol md sm="12" class="mb-sm-2 mb-0 d-md-down-none">
+              <div class="text-muted">ㅇㅇㅇ</div>
+              <strong>Average Rate (40.15%)</strong>
+              <CProgress class="progress-xs mt-2" :precision="1" :value="40" />
+            </CCol>
+          </CRow>
+        </CCardFooter>
+      </CCard>
+    </CCol>
+  </CRow>
 </template>
 
 <script>
@@ -164,5 +184,6 @@
 //     }
 //   }
 // };
-// </script>
+//
+</script>
 
