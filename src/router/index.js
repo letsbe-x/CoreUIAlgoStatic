535043645c696563a10ba93ab5ddca01e90370f5
Xxings <10years.shimhyeonggwan@gmail.com>
2020-02-19 18:38:11 +0900

    beforeEnter : user isexsist?

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-
+// @TODO : action으로 변경
+import axios from "axios";
+const _SERVER = "http://13.125.147.223:8080";
 // Containers
 const TheContainer = () => import("@/containers/TheContainer");
 
@@ -10,37 +12,7 @@ const Dashboard = () => import("@/views/Dashboard");
 const Feedback = () => import("@/views/FeedbackPage");
 const Recommend = () => import("@/views/RecommendAlgo");
 
-// const Colors = () => import("@/views/theme/Colors");
-// const Typography = () => import("@/views/theme/Typography");
-
-// const Charts = () => import("@/views/charts/Charts");
-// const Widgets = () => import("@/views/widgets/Widgets");
-
-// // Views - Components
-// const Cards = () => import("@/views/base/Cards");
-// const Forms = () => import("@/views/base/Forms");
-// const Switches = () => import("@/views/base/Switches");
-// const Tables = () => import("@/views/base/Tables");
-// const Tabs = () => import("@/views/base/Tabs");
-// const Breadcrumbs = () => import("@/views/base/Breadcrumbs");
-// const Carousels = () => import("@/views/base/Carousels");
-// const Collapses = () => import("@/views/base/Collapses");
-// const Jumbotrons = () => import("@/views/base/Jumbotrons");
-// const ListGroups = () => import("@/views/base/ListGroups");
-// const Navs = () => import("@/views/base/Navs");
-// const Navbars = () => import("@/views/base/Navbars");
-// const Paginations = () => import("@/views/base/Paginations");
-// const Popovers = () => import("@/views/base/Popovers");
-// const ProgressBars = () => import("@/views/base/ProgressBars");
-// const Tooltips = () => import("@/views/base/Tooltips");
-
-// // Views - Buttons
-// const StandardButtons = () => import("@/views/buttons/StandardButtons");
-// const ButtonGroups = () => import("@/views/buttons/ButtonGroups");
-// const Dropdowns = () => import("@/views/buttons/Dropdowns");
-// const BrandButtons = () => import("@/views/buttons/BrandButtons");
-
-// // Views - Icons
+// Views - Icons
 // const CoreUIIcons = () => import("@/views/icons/CoreUIIcons");
 // const Brands = () => import("@/views/icons/Brands");
 // const Flags = () => import("@/views/icons/Flags");
@@ -67,7 +39,14 @@ export default new Router({
   // mode: "history",
   linkActiveClass: "active",
   scrollBehavior: () => ({ y: 0 }),
-  routes: configRoutes()
+  routes: configRoutes(),
+  beforeEnter: function(to, from, next) {
+    // 인증 값 검증 로직 추가
+    console.warn(to);
+    console.warn(from);
+    console.warn(next);
+    next("/");
+  }
 });
 
 function configRoutes() {
@@ -75,12 +54,21 @@ function configRoutes() {
     {
       path: "/",
       name: "Search",
-      component: Search,
+      component: Search
     },
     {
       path: "/:user_id",
       redirect: "/:user_id/dashboard",
       name: "Home",
+      beforeEnter: function(to, from, next) {
+        // 인증 값 검증 로직 추가
+        let user_id = to.params.user_id;
+        let flag = axios
+          .get(`${_SERVER}/isUser/${user_id}`)
+          .then(res => res.data);
+        if (flag) next("/");
+        else next();
+      },
       component: TheContainer,
       children: [
         {
@@ -101,239 +89,8 @@ function configRoutes() {
           component: Recommend,
           props: true
         }
-        // {
-        //   path: "theme",
-        //   redirect: "/theme/colors",
-        //   name: "Theme",
-        //   component: {
-        //     render(c) {
-        //       return c("router-view");
-        //     }
-        //   },
-        //   children: [
-        //     {
-        //       path: "colors",
-        //       name: "Colors",
-        //       component: Colors
-        //     },
-        //     {
-        //       path: "typography",
-        //       name: "Typography",
-        //       component: Typography
-        //     }
-        //   ]
-        // },
-        // {
-        //   path: "charts",
-        //   name: "Charts",
-        //   component: Charts
-        // },
-        // {
-        //   path: "widgets",
-        //   name: "Widgets",
-        //   component: Widgets
-        // },
-        // {
-        //   path: "users",
-        //   meta: { label: "Users" },
-        //   component: {
-        //     render(c) {
-        //       return c("router-view");
-        //     }
-        //   },
-        //   children: [
-        //     {
-        //       path: "",
-        //       component: Users
-        //     },
-        //     {
-        //       path: ":id",
-        //       meta: { label: "User Details" },
-        //       name: "User",
-        //       component: User
-        //     }
-        //   ]
-        // },
-        // {
-        //   path: "base",
-        //   redirect: "/base/cards",
-        //   name: "Base",
-        //   component: {
-        //     render(c) {
-        //       return c("router-view");
-        //     }
-        //   },
-        //   children: [
-        //     {
-        //       path: "cards",
-        //       name: "Cards",
-        //       component: Cards
-        //     },
-        //     {
-        //       path: "forms",
-        //       name: "Forms",
-        //       component: Forms
-        //     },
-        //     {
-        //       path: "switches",
-        //       name: "Switches",
-        //       component: Switches
-        //     },
-        //     {
-        //       path: "tables",
-        //       name: "Tables",
-        //       component: Tables
-        //     },
-        //     {
-        //       path: "tabs",
-        //       name: "Tabs",
-        //       component: Tabs
-        //     },
-        //     {
-        //       path: "breadcrumbs",
-        //       name: "Breadcrumbs",
-        //       component: Breadcrumbs
-        //     },
-        //     {
-        //       path: "carousels",
-        //       name: "Carousels",
-        //       component: Carousels
-        //     },
-        //     {
-        //       path: "collapses",
-        //       name: "Collapses",
-        //       component: Collapses
-        //     },
-        //     {
-        //       path: "jumbotrons",
-        //       name: "Jumbotrons",
-        //       component: Jumbotrons
-        //     },
-        //     {
-        //       path: "list-groups",
-        //       name: "List Groups",
-        //       component: ListGroups
-        //     },
-        //     {
-        //       path: "navs",
-        //       name: "Navs",
-        //       component: Navs
-        //     },
-        //     {
-        //       path: "navbars",
-        //       name: "Navbars",
-        //       component: Navbars
-        //     },
-        //     {
-        //       path: "paginations",
-        //       name: "Paginations",
-        //       component: Paginations
-        //     },
-        //     {
-        //       path: "popovers",
-        //       name: "Popovers",
-        //       component: Popovers
-        //     },
-        //     {
-        //       path: "progress-bars",
-        //       name: "Progress Bars",
-        //       component: ProgressBars
-        //     },
-        //     {
-        //       path: "tooltips",
-        //       name: "Tooltips",
-        //       component: Tooltips
-        //     }
       ]
     },
-    //   {
-    //     path: "buttons",
-    //     redirect: "/buttons/standard-buttons",
-    //     name: "Buttons",
-    //     component: {
-    //       render(c) {
-    //         return c("router-view");
-    //       }
-    //     },
-    //     children: [
-    //       {
-    //         path: "standard-buttons",
-    //         name: "Standard Buttons",
-    //         component: StandardButtons
-    //       },
-    //       {
-    //         path: "button-groups",
-    //         name: "Button Groups",
-    //         component: ButtonGroups
-    //       },
-    //       {
-    //         path: "dropdowns",
-    //         name: "Dropdowns",
-    //         component: Dropdowns
-    //       },
-    //       {
-    //         path: "brand-buttons",
-    //         name: "Brand Buttons",
-    //         component: BrandButtons
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     path: "icons",
-    //     redirect: "/icons/coreui-icons",
-    //     name: "CoreUI Icons",
-    //     component: {
-    //       render(c) {
-    //         return c("router-view");
-    //       }
-    //     },
-    //     children: [
-    //       {
-    //         path: "coreui-icons",
-    //         name: "Icons library",
-    //         component: CoreUIIcons
-    //       },
-    //       {
-    //         path: "brands",
-    //         name: "Brands",
-    //         component: Brands
-    //       },
-    //       {
-    //         path: "flags",
-    //         name: "Flags",
-    //         component: Flags
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     path: "notifications",
-    //     redirect: "/notifications/alerts",
-    //     name: "Notifications",
-    //     component: {
-    //       render(c) {
-    //         return c("router-view");
-    //       }
-    //     },
-    //     children: [
-    //       {
-    //         path: "alerts",
-    //         name: "Alerts",
-    //         component: Alerts
-    //       },
-    //       {
-    //         path: "badges",
-    //         name: "Badges",
-    //         component: Badges
-    //       },
-    //       {
-    //         path: "modals",
-    //         name: "Modals",
-    //         component: Modals
-    //       }
-    //     ]
-    //   }
-    // ]
-    // },
     {
       path: "/pages",
       redirect: "/pages/404",
